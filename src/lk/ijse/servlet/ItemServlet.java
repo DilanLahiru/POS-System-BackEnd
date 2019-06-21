b536bd115f7e3d029339ef03890d2be4c25751f3
dilan <dilanlahiruijse@gmail.com>
2019-06-21 22:35:11 +0530

    modification Item servlet class

--- src/lk/ijse/servlet/ItemServlet.java
+++ src/lk/ijse/servlet/ItemServlet.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 @WebServlet(urlPatterns = "/items")
 public class ItemServlet extends HttpServlet {
@@ -60,5 +59,59 @@ public class ItemServlet extends HttpServlet {
 
         }
 
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        System.out.println("doPost Working...");
+        JsonReader reader = Json.createReader(req.getReader());
+        resp.setContentType("application/json");
+
+        PrintWriter out = resp.getWriter();
+
+        Connection connection = null;
+
+        try {
+            JsonObject item = reader.readObject();
+
+            String code = item.getString("code");
+            String description = item.getString("description");
+            String unitPrice = item.getString("unitPrice");
+            String qtyonHand = item.getString("qtyOnHand");
+
+            System.out.println("code : " + code);
+            System.out.println("description : " + description);
+            System.out.println("unitPrice : " + unitPrice);
+            System.out.println("qtyOnHand : " + qtyonHand);
+
+
+            connection = ds.getConnection();
+
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO Customer VALUES (?,?,?,?)");
+            pstm.setObject(1, code);
+            pstm.setObject(2, description);
+            pstm.setObject(3, unitPrice);
+            pstm.setObject(4, qtyonHand);
+
+            boolean result = pstm.executeUpdate() > 0;
+
+            if (result) {
+
+                out.println("true");
+            } else {
+                out.println("false");
+            }
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            out.println("false");
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            out.close();
+        }
     }
 }
